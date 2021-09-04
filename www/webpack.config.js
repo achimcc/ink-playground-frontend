17068bb0307145d5baedc10a46fb6126c7740089
achimcc <achim@parity.io>
2021-09-04 11:01:04 +0200

    compiles to wasm, fix cross origin reference

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -92,7 +92,7 @@ module.exports = {
   devServer: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "cross-origin",
+      "Cross-Origin-Opener-Policy": "same-origin",
     },
   },
 };
