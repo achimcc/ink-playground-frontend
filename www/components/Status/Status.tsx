d5d9d11f0da7e2d5374aff4e40ce7ca2d12fd1c8
achimcc <achim@parity.io>
2021-09-23 15:54:52 +0200

    Status bar styling

--- www/components/Status/Status.tsx
+++ www/components/Status/Status.tsx
@@ -6,32 +6,20 @@ import { ClipLoader } from "react-spinners";
 const Status = () => {
   const { isLoading } = usePlayground();
   return (
-    <div
-      style={{
-        border: "1px solid black",
-        display: "table",
-      }}
-      className="bg-gray-600 text-primary w-full"
-    >
+    <div className="table bg-primary text-primary w-full">
       {isLoading ? (
         <div>
-          <div style={{ padding: "1px 2px", display: "table-cell" }}>
-            <ClipLoader size={12} />
+          <div className="px-0.5 py-1 table-cell">
+            <ClipLoader color={"text-primary"} size={12} />
           </div>
-          <div style={{ display: "table-cell" }}>Loading Rust-Analyzer...</div>
+          <div className="table-cell">Loading Rust-Analyzer...</div>
         </div>
       ) : (
         <>
-          <div
-            style={{
-              padding: "1px 2px",
-              display: "table-cell",
-              width: "18px",
-            }}
-          >
+          <div className="px-0.5 py-1 table-cell w-5">
             <FaCheckCircle className="text-green-500" />
           </div>
-          <div style={{ display: "table-cell" }}>Rust Analyzer Ready</div>
+          <div className="table-cell">Rust Analyzer Ready</div>
         </>
       )}
     </div>
