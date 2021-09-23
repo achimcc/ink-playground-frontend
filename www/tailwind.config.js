62e47d12bddf58a93946dbe6cb0aa081d3c72e01
erler <mail@michelerler.com>
2021-09-23 16:34:01 +0200

    updating tailwind config

--- www/tailwind.config.js
+++ www/tailwind.config.js
@@ -14,6 +14,29 @@ module.exports = {
       secondary: "#ffed4a",
       danger: "#e3342f",
     }),
+    extend: {
+      colors: {
+        elevation: {
+          0: '#1A1D1F',
+          1: '#202529',
+          2: '#242A2E',
+          3: '#2F373D'
+        },
+        gray: {
+          50: '#F5F6FA',
+          100: '#F0F1F7',
+          200: '#E6E7F0',
+          300: '#D3D4DB',
+          400: '#9597A6',
+          500: '#737480',
+          600: '#4D4F5C',
+          700: '#3C3D47',
+          800: '#2B2C33',
+          850: '#1D2124',
+          900: '#1A1D1F',
+        }
+      }
+    }
   },
   variants: {
     extend: {},
