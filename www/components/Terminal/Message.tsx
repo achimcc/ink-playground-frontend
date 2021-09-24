491840e6a10e8106d52a87d2499fffde888606a2
achimcc <achim@parity.io>
2021-09-24 09:28:52 +0200

    add prompt colors to terminal

--- www/components/Terminal/Message.tsx
+++ www/components/Terminal/Message.tsx
@@ -5,11 +5,17 @@ interface Props {
   message: Message;
 }
 
+const severityColors = {
+  info: "text-yellow-300",
+  success: "text-green-300",
+  error: "text-red-300",
+};
+
 const TerminalMessage = ({ message: m }: Props) => {
   return (
     <>
       <div className="mt-4 flex">
-        <span className="text-yellow-300">{m.prompt} &gt;</span>
+        <span className={severityColors[m.severity]}>{m.prompt} &gt;</span>
         <p className="flex-1 typing items-center pl-2">{m.text}</p>
       </div>
     </>
